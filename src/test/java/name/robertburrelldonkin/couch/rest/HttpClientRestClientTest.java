306c2353157d53c1c90adc4b9fafd9522a203206
Robert Burrell Donkin <code@robertburrelldonkin.name>
2012-12-23 11:50:41 +0000

    Looks like we'll be added a few classes to implement rest connectivity, so create some space by repackaging.

---
+++ src/test/java/name/robertburrelldonkin/couch/rest/HttpClientRestClientTest.java
@@ -0,0 +1,59 @@
+/*
+* 	Copyright 2012 Robert Burrell Donkin robertburrelldonkin.name
+* 
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+*/
+package name.robertburrelldonkin.couch.rest;
+
+import static org.mockito.Mockito.*;
+
+import name.robertburrelldonkin.couch.rest.HttpClientRestClient;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ClientConnectionManager;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class HttpClientRestClientTest {
+	
+	@Mock
+	HttpClient client;
+	
+	@Mock
+	ClientConnectionManager connectionManager;
+	
+	HttpClientRestClient subject;
+	
+	@Before
+	public void before() {
+		this.subject = new HttpClientRestClient(client);
+	}
+	
+	@Test
+	public void testShutdownShutsDownClient() {
+		when(client.getConnectionManager()).thenReturn(connectionManager);
+		this.subject.shutdown();
+		verify(this.connectionManager).shutdown();
+	}
+	
+	@Test
+	public void smokeShutdownWhenManagerIsNull() {
+		when(client.getConnectionManager()).thenReturn(null);
+		this.subject.shutdown();
+	}
+}
